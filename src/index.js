887738eb4307dd5935a090d32df43beed6e6e988
srh0820 <srh96820@gmail.com>
2024-07-08 17:46:33 +0900

    props 활용하기

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './Child'; // 상대경로는 모두 src 폴더에서 찾기
+import App from './App';
+
+const myinfo = [{
+  price : 50000,
+  pronm : "신발",
+  protext : "신발입니다1"
+},
+{
+  price : 100000,
+  pronm : "신발",
+  protext : "신발입니다2"
+}]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App msg="메세지" num="10" cls="bg-dark fixed-top text-white"/>
+  <>
+    <App sendinfo ={myinfo[0]}></App>
+    <App sendinfo ={myinfo[1]}></App>
+  </>
 );
