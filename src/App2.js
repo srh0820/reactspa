5b4def1246d8b3049e3486ccaf229ed8fd39ae7a
srh0820 <srh96820@gmail.com>
2024-07-10 11:49:42 +0900

    ${props =>}

--- src/App2.js
+++ src/App2.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Title , Abtn } from './commonui/ui'
+import { Title , Abtn , Button , Buttoncolor , Buttonany } from './commonui/ui'
 
 function App2(){
     return (
@@ -15,6 +15,10 @@ function App2(){
             타이틀입니다
             <Abtn>응모하기</Abtn>
          </h2>
+            <Button>일반 버튼</Button>
+            <Button primary>프라이머리 버튼</Button>
+            <Buttoncolor bgcolor="red" textcolor="white">props 배경색 글자색 넣기</Buttoncolor>
+            <Buttonany bgcolor="black" textcolor="white" fonstsize="50px" fontfamily="맑은 고딕">변수 4개</Buttonany>
         </div>
       );
     }
--- src/commonui/ui.js
+++ src/commonui/ui.js
@@ -18,3 +18,38 @@ export const Abtn = styled.a`
       color:white;
     }
 `;
+
+export const Button = styled.button`
+  background: ${props => props.primary ? 'palevioletred' : 'white'};
+  color: ${props => props.primary ? 'white' : 'palevioletred'};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  &:hover {
+    background: ${props => props.primary ? 'white' : 'palevioletred'};
+    color: ${props => props.primary ? 'palevioletred' : 'white'};
+  }
+`;
+
+export const Buttoncolor = styled.button`
+  background: ${props => props.bgcolor};
+  color: ${props => props.textcolor};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+export const Buttonany = styled.button`
+  background: ${props => props.bgcolor};
+  color: ${props => props.textcolor};
+  font-size: ${props => props.fonstsize};
+  font-family : ${props => props.fontfamily};
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
