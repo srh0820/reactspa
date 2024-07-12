f5226d7d40286026d03ac134d34070c18a093a55
srh0820 <srh96820@gmail.com>
2024-07-12 11:09:37 +0900

    useEffect, map 사용해서 bgcolor 전환

--- src/App6use.js
+++ src/App6use.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [ count, setCount ] = useState(0);
-  const bgcolor = ["red", "green", "blue"];
+  const bgcolor = ["red", "green", "blue", "pink"];
 
   // useEffect 프로토타입
   // useEffect(()=>{}, [])
@@ -13,17 +13,28 @@ function App() {
     // count 가 변경되면 그때마다 함수를 실행하겠다.
 
   return (
-    <div className="App">
+    <>
+      <div className="App">
+        <button onClick={() => {
+          setCount( count < bgcolor.length -1 ? count + 1 : 0  ); // 대입식 절대 불가! 연산식만
+        }}> {count} 증가 </button>
 
-      <button onClick={() => {
-        setCount( count < bgcolor.length -1 ? count + 1 : 0  ); // 대입식 절대 불가! 연산식만
-      }}> {count} 증가 </button>
-
-      <button onClick={() => {
-        setCount( count > 0 ? count - 1 : bgcolor.length -1 );
-      }}> {count} 감소 </button>
-
-    </div>
+        <button onClick={() => {
+          setCount( count > 0 ? count - 1 : bgcolor.length -1 );
+        }}> {count} 감소 </button>
+      </div>
+      <div>
+        {/* <button>1</button><button>2</button><button>3</button><button>4</button> */}
+        {
+          // this 의 여부에 따라서 function 또는 화살표함수로 결정
+          bgcolor.map((v, i) => {
+            return(
+              <button onClick={()=> { setCount(i) }}>{v}</button>
+            )
+          })
+        }
+      </div>
+    </>
   );
 }
 
